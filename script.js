c8c52a2c86452e4b8047941839f0a2f8344ebdf5
Deepak Modanwal <118596712+deepakmod@users.noreply.github.com>
2023-04-24 13:45:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function getFormvalue() {
-    //Write your code here
+let input=document.getElementsByTagName("input");
 
+function getFormvalue() {
+    var first=input[0].value;
+    var last=input[1].value;
+    alert(first+" "+last)
 }
